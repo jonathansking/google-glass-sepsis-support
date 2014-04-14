4c5a2cf1c91ef52a54f2de69f2b1e53061a6a28f
Casey Wilson <cg.wilson33.3@gmail.com>
2014-04-13 22:01:55 -0700

    Added OverviewActivity, works! Activity stack is handle correctly now Renamed MenuActivity->WelcomeActivity

--- SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/OverviewActivity.java
+++ SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/OverviewActivity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 The Android Open Source Project
+
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package edu.ucdavis.glass.sepsis.support;
 
 import com.google.android.glass.touchpad.Gesture;
@@ -30,61 +32,38 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 
-/**
- * Activity showing the options menu.
- */
-public class MenuActivity extends Activity {
+public class OverviewActivity extends Activity {
 	private GestureDetector mGestureDetector;
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.welcome_screen);
-//        setContentView(R.layout.recent_patient);
         setContentView(R.layout.overview);
+        mGestureDetector = createGestureDetector(this);
     }
 	
 	private GestureDetector createGestureDetector(Context context) {
     GestureDetector gestureDetector = new GestureDetector(context);
-        //Create a base listener for generic gestures
+        // create a base listener for generic gestures
         gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
             @Override
             public boolean onGesture(Gesture gesture) {
                 if (gesture == Gesture.TAP) {
-                    // do something on tap
                     return true;
                 } else if (gesture == Gesture.TWO_TAP) {
-                    // do something on two finger tap
                     return true;
                 } else if (gesture == Gesture.SWIPE_RIGHT) {
-                    // do something on right (forward) swipe
-                	setContentView(R.layout.recent_patient);
                     return true;
                 } else if (gesture == Gesture.SWIPE_LEFT) {
-                    // do something on left (backwards) swipe
-                	setContentView(R.layout.recent_patient);
                     return true;
                 }
                 return false;
             }
         });
-//	        gestureDetector.setFingerListener(new GestureDetector.FingerListener() {
-//	            @Override
-//	            public void onFingerCountChanged(int previousCount, int currentCount) {
-//	              // do something on finger count changes
-//	            }
-//	        });
-//	        gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
-//	            @Override
-//	            public boolean onScroll(float displacement, float delta, float velocity) {
-//	                // do something on scrolling
-//	            }
-//	        });
+        
         return gestureDetector;
     }
 
-    /*
-     * Send generic motion events to the gesture detector
-     */
+    // send generic motion events to the gesture detector
     @Override
     public boolean onGenericMotionEvent(MotionEvent event) {
         if (mGestureDetector != null) {
--- SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/WelcomeActivity.java
+++ SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/WelcomeActivity.java
@@ -1,4 +1,23 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.ucdavis.glass.sepsis.support;
+
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 
 import android.app.Activity;
 import android.content.ComponentName;
@@ -12,16 +31,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 
-import com.google.android.glass.touchpad.Gesture;
-import com.google.android.glass.touchpad.GestureDetector;
-
-public class RecentPatientActivity extends Activity {
+public class WelcomeActivity extends Activity {
 	private GestureDetector mGestureDetector;
-	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recent_patient);
+        setContentView(R.layout.welcome_screen);
         mGestureDetector = createGestureDetector(this);
     }
 	
@@ -37,10 +52,10 @@ public class RecentPatientActivity extends Activity {
                 } else if (gesture == Gesture.TWO_TAP) {
                     return true;
                 } else if (gesture == Gesture.SWIPE_RIGHT) {
-                	finish();
+                	startActivity( new Intent(getApplicationContext(), RecentPatientActivity.class) );
                     return true;
                 } else if (gesture == Gesture.SWIPE_LEFT) {
-                	finish();
+                	startActivity( new Intent(getApplicationContext(), RecentPatientActivity.class) );
                     return true;
                 }
                 return false;
@@ -50,7 +65,7 @@ public class RecentPatientActivity extends Activity {
         return gestureDetector;
     }
 
-	// send generic motion events to the gesture detector
+    // send generic motion events to the gesture detector
     @Override
     public boolean onGenericMotionEvent(MotionEvent event) {
         if (mGestureDetector != null) {
