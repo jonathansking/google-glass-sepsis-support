038521a11ef232580252705a5e7b4b0fe291059f
Casey Wilson <cg.wilson33.3@gmail.com>
2014-04-13 20:14:46 -0700

    Updated MenuActivity with swiping controls (not working)

--- SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/MenuActivity.java
+++ SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/MenuActivity.java
@@ -13,11 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package edu.ucdavis.glass.sepsis.support;
+
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
@@ -25,16 +28,68 @@ import android.os.IBinder;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 
 /**
  * Activity showing the options menu.
  */
 public class MenuActivity extends Activity {
+	private GestureDetector mGestureDetector;
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.welcome_screen);
 //        setContentView(R.layout.recent_patient);
         setContentView(R.layout.overview);
+    }
+	
+	private GestureDetector createGestureDetector(Context context) {
+    GestureDetector gestureDetector = new GestureDetector(context);
+        //Create a base listener for generic gestures
+        gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
+            @Override
+            public boolean onGesture(Gesture gesture) {
+                if (gesture == Gesture.TAP) {
+                    // do something on tap
+                    return true;
+                } else if (gesture == Gesture.TWO_TAP) {
+                    // do something on two finger tap
+                    return true;
+                } else if (gesture == Gesture.SWIPE_RIGHT) {
+                    // do something on right (forward) swipe
+                	setContentView(R.layout.recent_patient);
+                    return true;
+                } else if (gesture == Gesture.SWIPE_LEFT) {
+                    // do something on left (backwards) swipe
+                	setContentView(R.layout.recent_patient);
+                    return true;
+                }
+                return false;
+            }
+        });
+//	        gestureDetector.setFingerListener(new GestureDetector.FingerListener() {
+//	            @Override
+//	            public void onFingerCountChanged(int previousCount, int currentCount) {
+//	              // do something on finger count changes
+//	            }
+//	        });
+//	        gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
+//	            @Override
+//	            public boolean onScroll(float displacement, float delta, float velocity) {
+//	                // do something on scrolling
+//	            }
+//	        });
+        return gestureDetector;
+    }
+
+    /*
+     * Send generic motion events to the gesture detector
+     */
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        if (mGestureDetector != null) {
+            return mGestureDetector.onMotionEvent(event);
+        }
+        return false;
     }
 }
