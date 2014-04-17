57e60800bbf464f2ade1d6f51b0422826f024e4f
NguyenHoc <hnvo@ucdavis.edu>
2014-04-17 10:52:55 -0700

    Added CardScrollView for Recent Patient (not working yet!)

--- SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/RecentPatientActivity.java
+++ SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/RecentPatientActivity.java
@@ -1,61 +1,104 @@
 package edu.ucdavis.glass.sepsis.support;
 
-import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.MotionEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Object;
 
-import com.google.android.glass.touchpad.Gesture;
-import com.google.android.glass.touchpad.GestureDetector;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import com.google.android.glass.app.Card;
+import com.google.android.glass.widget.CardScrollAdapter;
+import com.google.android.glass.widget.CardScrollView;
 
 public class RecentPatientActivity extends Activity {
-	private GestureDetector mGestureDetector;
-	
-	@Override
+
+    private ArrayList<Card> mCards;
+    private CardScrollView mCardScrollView;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recent_patient);
-        mGestureDetector = createGestureDetector(this);
-    }
-	
-	private GestureDetector createGestureDetector(Context context) {
-    GestureDetector gestureDetector = new GestureDetector(context);
-        // create a base listener for generic gestures
-        gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
-            @Override
-            public boolean onGesture(Gesture gesture) {
-                if (gesture == Gesture.TAP) {
-                	startActivity( new Intent(getApplicationContext(), OverviewActivity.class) );
-                    return true;
-                } else if (gesture == Gesture.TWO_TAP) {
-                    return true;
-                } else if (gesture == Gesture.SWIPE_RIGHT) {
-                	finish();
-                    return true;
-                } else if (gesture == Gesture.SWIPE_LEFT) {
-                	finish();
-                    return true;
-                }
-                return false;
-            }
-        });
-        
-        return gestureDetector;
+
+        createCards();
+
+        mCardScrollView = new CardScrollView(this);
+        PatientCardScrollAdapter adapter = new PatientCardScrollAdapter();
+        mCardScrollView.setAdapter(adapter);
+        mCardScrollView.activate();
+        setContentView(mCardScrollView);
     }
 
-	// send generic motion events to the gesture detector
-    @Override
-    public boolean onGenericMotionEvent(MotionEvent event) {
-        if (mGestureDetector != null) {
-            return mGestureDetector.onMotionEvent(event);
+    private void createCards() {
+        mCards = new ArrayList<Card>();
+
+        Card newcard;
+
+        newcard = new Card(this);
+        newcard.setText("This card has a footer.");
+        newcard.setFootnote("I'm the footer!");
+//        newcard.setImageLayout(Card.ImageLayout.FULL);
+        mCards.add(newcard);
+
+        newcard = new Card(this);
+        newcard.setText("This card has a puppy background image.");
+        newcard.setFootnote("How can you resist?");
+//        newcard.setImageLayout(Card.ImageLayout.FULL);
+        mCards.add(newcard);
+
+        newcard = new Card(this);
+        newcard.setText("This card has a mosaic of puppies.");
+        newcard.setFootnote("Aren't they precious?");
+//        newcard.setImageLayout(Card.ImageLayout.FULL);
+        mCards.add(newcard);
+    }
+    
+    private class PatientCardScrollAdapter extends CardScrollAdapter {
+
+        @Override
+        public int getPosition(Object item) {
+            return mCards.indexOf(item);
         }
-        return false;
+
+        @Override
+        public int getCount() {
+            return mCards.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mCards.get(position);
+        }
+
+        /**
+         * Returns the amount of view types.
+         */
+        @Override
+        public int getViewTypeCount() {
+            return Card.getViewTypeCount();
+        }
+
+        /**
+         * Returns the view type of this card so the system can figure out
+         * if it can be recycled.
+         */
+        @Override
+        public int getItemViewType(int position){
+            return mCards.get(position).getItemViewType();
+        }
+
+        @Override
+        public View getView(int position, View convertView,
+                ViewGroup parent) {
+            return  mCards.get(position).getView(convertView, parent);
+        }
+    	
+//    	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+//        	startActivity( new Intent(getApplicationContext(), OverviewActivity.class) );
+//        }
     }
 }
