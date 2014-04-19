7f486da04383534ac4fddec330d9242818350b61
George Pantazes <gwpantazes@ucdavis.edu>
2014-04-18 17:12:09 -0700

    On this branch, split from working recentpatientsview, added some android code to interact with Sushil's web server onto the overview page so we can grab a piece of data band display it there

--- SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/OverviewActivity.java
+++ SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/OverviewActivity.java
@@ -20,6 +20,11 @@ package edu.ucdavis.glass.sepsis.support;
 import com.google.android.glass.touchpad.Gesture;
 import com.google.android.glass.touchpad.GestureDetector;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
@@ -31,6 +36,17 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class OverviewActivity extends Activity {
 	private GestureDetector mGestureDetector;
@@ -39,7 +55,55 @@ public class OverviewActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.overview);
         mGestureDetector = createGestureDetector(this);
-    }
+        
+        //# Just for testing, allow network access in the main thread
+        //# NEVER use this is productive code
+        /*StrictMode.ThreadPolicy policy = new StrictMode.
+        ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);*/ 
+        
+        
+        String sepsisDBReturnString = readFromDB();
+        try {
+          JSONArray jsonArray = new JSONArray(sepsisDBReturnString);
+          Log.i(OverviewActivity.class.getName(),
+              "Number of entries " + jsonArray.length());
+          for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            Log.i(OverviewActivity.class.getName(), jsonObject.getString("text"));
+          }
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+
+      public String readFromDB() {
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet("http://ihuman.SUSHIL'S_WEBSITE_HAHAHAHAHAHAHAHAHAHAHAHAHAHAHA.json");
+        try {
+          HttpResponse response = client.execute(httpGet);
+          StatusLine statusLine = response.getStatusLine();
+          int statusCode = statusLine.getStatusCode();
+          if (statusCode == 200) {
+            HttpEntity entity = response.getEntity();
+            InputStream content = entity.getContent();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+            String line;
+            while ((line = reader.readLine()) != null) {
+              builder.append(line);
+            }
+          } else {
+            Log.e(OverviewActivity.class.toString(), "Failed to download file");
+          }
+        } catch (ClientProtocolException e) {
+          e.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        return builder.toString();
+      }
+    
 	
 	private GestureDetector createGestureDetector(Context context) {
     GestureDetector gestureDetector = new GestureDetector(context);
@@ -71,4 +135,6 @@ public class OverviewActivity extends Activity {
         }
         return false;
     }
+    
+    
 }
