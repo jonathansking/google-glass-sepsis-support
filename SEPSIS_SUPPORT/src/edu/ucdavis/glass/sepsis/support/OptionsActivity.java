0554cd657725cceaba7bcf6af465d1fa1f343f95
Casey Wilson <cg.wilson33.3@gmail.com>
2014-04-29 23:46:39 -0700

    Added menu to Options actvity via options_menu.xml Should open menu and allow setting values (just sets them to 10 for now)

--- SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/OptionsActivity.java
+++ SEPSIS_SUPPORT/src/edu/ucdavis/glass/sepsis/support/OptionsActivity.java
@@ -3,6 +3,9 @@ package edu.ucdavis.glass.sepsis.support;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 
 import com.google.android.glass.touchpad.Gesture;
@@ -10,6 +13,18 @@ import com.google.android.glass.touchpad.GestureDetector;
 
 public class OptionsActivity extends Activity 
 {
+	// structure to save options
+	public class Options 
+	{
+		public int screenTimeout;
+		public int numberOfRecentPatients;
+		
+		public Options() {
+			screenTimeout = 30;
+			numberOfRecentPatients = 5;
+		}
+	}
+	
 	private GestureDetector mGestureDetector;
 
 	@Override
@@ -20,6 +35,37 @@ public class OptionsActivity extends Activity
         setContentView(R.layout.options);
         mGestureDetector = createGestureDetector(this);
     }
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.options_menu, menu);
+	    return true;
+	}
+
+	public boolean onMenuItemClick(MenuItem item) 
+	{
+	    switch (item.getItemId()) 
+	    {
+	        case R.id.set_screen_timeout:
+	        	Global.options.screenTimeout = selectValueStartingAt( Global.options.screenTimeout );
+	            return true;
+	        case R.id.set_num_recent_patients:
+	        	Global.options.numberOfRecentPatients = = selectValueStartingAt( Global.options.screenTimeout );
+	            return true;
+	        case R.id.reset_options:
+	        	Global.options = new Options();
+	            return true;
+	        default:
+	            return false;
+	    }
+	}
+	
+	int selectValueStartingAt( int s ){
+		// make a bunch of cards allow the user to scroll through them and select one
+		// return the selected value
+		return 10; // for now
+	}
 	
 	private GestureDetector createGestureDetector(Context context) 
 	{
@@ -33,6 +79,7 @@ public class OptionsActivity extends Activity
                 if (gesture == Gesture.TAP) 
                 {
                 	// bring up menu
+                	openOptionsMenu();
                     return true;
                 } 
                 else if (gesture == Gesture.TWO_TAP) 
@@ -66,15 +113,4 @@ public class OptionsActivity extends Activity
         
         return false;
     }
-	
-	public class Options 
-	{
-		public int screenTimeout;
-		public int numberOfRecentPatients;
-		
-		public Options() {
-			screenTimeout = 30;
-			numberOfRecentPatients = 5;
-		}
-	}
 }
